840fef1cb2b2e15e5d76aaca6cfc646af9619ea8
Mayuresh <mayureshkn99@gmail.com>
2023-02-26 19:48:25 -0500

    Added next_card logic

--- test.py
+++ test.py
@@ -11,11 +11,14 @@ class Game:
 
         # Initializing Cards
         self.cards = ["Taco", "Cat", "Goat", "Cheese", "Pizza"]*11
-        self.cards.extend(["Narwhal"]*3)
+        self.cards.extend(["Narwhal"]*9)
         random.shuffle(self.cards)
 
         # Loading card Images
         self.card_images = {}
+        image = Image.open(f"images/cover.jpg")
+        image = image.resize((200, 300))
+        self.card_images["Cover"] = ImageTk.PhotoImage(image)
         for card in set(self.cards):
             image = Image.open(f"images/{card.lower()}.jpg")
             self.card_images[card] = ImageTk.PhotoImage(image)
@@ -42,13 +45,14 @@ class Game:
         
     def start_game(self):
         self.GAME = True
+        self.HISTORY = []
         self.player_label = self.score_label = {}
 
         # Adding Player Labels and Cards Left
         for i in range(4):
             name = self.entry[i].get()+": "
             self.entry[i].grid_forget()
-            self.player_label[i] = Label(root, text=name, font=("Helvetica", 24, "bold"))
+            self.player_label[i] = Button(root, text=name, font=("Helvetica", 24, "bold"), command=lambda i=i :self.player_loses(i))
             self.player_label[i].grid(row=i, column=0, pady=20)
             self.score_label[i] = Label(root, text="16 cards left", font=("Helvetica", 24, "bold"))
             self.score_label[i].grid(row=i, column=1, pady=20)
@@ -58,11 +62,15 @@ class Game:
         self.reset_button = Button(root, text="Reset", font=("Helvetica", 16), command=self.reset_game)
         self.reset_button.grid(row=5, column=0, columnspan=2, padx=20, pady=10)
 
-        self.current_card_label = Label(root, text=self.current_card, font=("Helvetica", 24, "bold"))
-        self.current_card_label.grid(row=0, column=3, rowspan=4, padx=20, pady=20)
+        self.current_card_image = Label(root, image=self.card_images["Cover"])
+        self.current_card_image.grid(row=0, column=3, rowspan=4, padx=20, pady=20)
         
         # bind the function to the space bar event
         root.bind("<space>", self.next_card)
+
+
+    def player_loses(self, player):
+        pass
 
         
     def reset_game(self):
@@ -72,7 +80,16 @@ class Game:
     def next_card(self, event):
         if self.GAME == False:
             return
-        self.current_card = self.player_cards[self.TURN].pop()
+        current_card = self.player_cards[self.TURN].pop(0)
+        
+        # Updating the Score
+        self.score_label[self.TURN].config(text=f"{len(self.player_cards[self.TURN])} cards left")
+
+        # Updating the card image
+        self.current_card_image.config(image=self.card_images[current_card])
+        self.HISTORY.append(current_card)
+
+        # Updating the turn
         self.TURN = (self.TURN+1)%4
 
 
